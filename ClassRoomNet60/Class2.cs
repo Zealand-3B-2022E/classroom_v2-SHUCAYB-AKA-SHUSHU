ab0046c1dc9aa702f784373737ba50df6a488e78
SHUCAYB-AKA-SHUSHU <93126733+SHUCAYB-AKA-SHUSHU@users.noreply.github.com>
2022-09-05 14:32:59 +0200

    default Construcktor

---
+++ ClassRoomNet60/Class2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Classroom
+{
+	public Classroom()
+	{
+	public string Klassenavn { get; set; }
+	public List<Studerende> myClasslist = new List<studerende>();
+	public Datetime SemesterStart { get; set; }
+
+	public object klassenavn
+	{
+
+	}
+
+	public object List
+	{
+
+
+	}
+
+	public dateTime SemesterStart 
+	{
+	
+	
+	}
+
+	
+}
+}
