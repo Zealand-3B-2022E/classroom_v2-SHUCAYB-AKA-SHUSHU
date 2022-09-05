4cfe6acc5e0cbce42e7c47c54cf59a6d3ad13325
SHUCAYB-AKA-SHUSHU <93126733+SHUCAYB-AKA-SHUSHU@users.noreply.github.com>
2022-09-05 13:45:24 +0200

    Opgave 1 done

---
+++ ClassRoomNet60/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Student
+{
+	public Student()
+	{
+	public string Name { get; set; }
+	public int Birthmonths { get; set; }
+	public int Birthday { get; set; }
+
+	public Student()
+	{ }
+
+	public Student(string name,int birthmonths,int birthday) 
+	{
+		this.Name = name;
+		this.Birthmonths = birthmonths;
+		this.Birthday = birthday;
+	
+	}
+
+
+	}
+}
